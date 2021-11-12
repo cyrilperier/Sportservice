f30fd9e9d15ed9709ab0a6dbebd00796f88d9c39
adrien <brookdroger@gmail.com>
2021-11-12 13:29:18 -0500

    creation du document user avec comme id le UID de l'utilisateur au moment du register + ajout du champ "title" au training personnalisé

--- app/src/main/java/com/uqac/pmm/HomeActivity.kt
+++ app/src/main/java/com/uqac/pmm/HomeActivity.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.uqac.pmm.ui.main.SectionsPagerAdapter
-import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home.*
-import com.google.firebase.auth.GetTokenResult
 
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.ktx.firestore
-import java.sql.Timestamp
+import java.io.Serializable
 
 
 class HomeActivity : AppCompatActivity() {
@@ -51,11 +48,14 @@ class HomeActivity : AppCompatActivity() {
         }
         val db = Firebase.firestore
         val training = hashMapOf(
+            "title" to "training 1",
             "exercices" to arrayListOf<String>("test5", "test6"),
             "dates" to arrayListOf<String>("23 novembre 2021 à 00:00:00 UTC-5\n"),
         )
         //ADD A TRAINING TO THE USER'S TRAINING COLLECTION
-        //addTraining(uid,training)
+        addTraining(uid,training)
+
+        //permet de get les doc de la collection training
         val docRef = db.collection("users").document("$uid")
             .collection("trainings")
         docRef.get()
@@ -70,7 +70,8 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    private fun addTraining(uid: String?, training: HashMap<String, ArrayList<String>>) {
+    //ADD A TRAINING TO THE USER'S TRAINING COLLECTION
+    private fun addTraining(uid: String?, training: HashMap<String, Serializable>) {
 
         val db = Firebase.firestore
         db.collection("users").document("$uid")
@@ -82,7 +83,6 @@ class HomeActivity : AppCompatActivity() {
             .addOnFailureListener { e ->
                 Log.d("TAG", "Error adding document", e)
             }
-
     }
 
     override fun onDestroy() {
