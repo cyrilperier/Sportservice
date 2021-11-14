1e310482eade366bc11d8240a733c4fc89672b50
cyrilperier <64652284+cyrilperier@users.noreply.github.com>
2021-11-14 18:23:47 -0500

    Implementation de la page calendrier

--- app/src/main/java/com/uqac/pmm/fragment/FragmentCalendar.kt
+++ app/src/main/java/com/uqac/pmm/fragment/FragmentCalendar.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uqac.pmm.R
 
 
 class FragmentCalendar : Fragment() {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
 }
--- app/src/main/java/com/uqac/pmm/fragment/FragmentCalendarDescription.kt
+++ app/src/main/java/com/uqac/pmm/fragment/FragmentCalendarDescription.kt
@@ -8,12 +8,10 @@ import android.view.ViewGroup
 import com.uqac.pmm.R
 
 
-class FragmentCalendarDescription : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        vg: ViewGroup?,
-        savedInstanceState: Bundle
-    ): View? {
-        return inflater.inflate(R.layout.fragment_calendar_description, vg, false)
+
+    class FragmentCalendarDescription : androidx.fragment.app.Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            return inflater.inflate(R.layout.fragment_calendar_description, container, false)
+        }
     }
-}
