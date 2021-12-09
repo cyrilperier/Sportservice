0216a9cce9098a2f25cb15deb442158c11d48a3a
adrien <brookdroger@gmail.com>
2021-12-09 03:34:53 -0500

    Mise à jour du dialog de creation d'entrainement personnalisé L'utilisateur peut maintenant ajouter des exos directement a la creation du training via un bouton "add exercise" qui ouvre une alert dialog avec tout les exos presents dans la nouvelle collection 'exercises' que j'ai crée au root du firestore Cliquer sur Ok dans la premiere dialog cree donc l'entrainement ainsi que la collection exercices (liée au training) et les documents liés aux exos avec pour documentID leur identifiant de la collection exercises comme ça si on ajoute des parametres dans la collection root exersices ça sera facile de les recuperer avec l'id de l'exo en question J'ai aussi designé les deux dialogs pour qu'elles pas trop moches c'etait compliqué vu que les AlertDialog sont créé programmatiquement mais bon j'ai fait au mieux donc j'ai fait des modifs dans le themes.xml ainsi que le colors
     - ADRIEN

--- app/src/main/java/com/uqac/pmm/DialogFragmentAddTraining.kt
+++ app/src/main/java/com/uqac/pmm/DialogFragmentAddTraining.kt
@@ -1,9 +1,11 @@
 package com.uqac.pmm
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,7 +13,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
@@ -20,23 +22,28 @@ import java.sql.Timestamp
 import java.util.*
 
 
-class DialogFragmentAddTraining() : DialogFragment(){
-
+class DialogFragmentAddTraining() : DialogFragment() {
+    val map = linkedMapOf<String, String>()
+    var selectedItems = ArrayList<Int>()
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-            val builder = AlertDialog.Builder(it)
+            val builder = AlertDialog.Builder(it,R.style.MyDialogTheme)
             // Get the layout inflater
             var inflater : LayoutInflater = requireActivity().layoutInflater
             var view : View = inflater.inflate(R.layout.add_training_to_user,null)
             var btn : Button = view.findViewById(R.id.pickDateBtn)
+            var btn2 : Button = view.findViewById(R.id.pickExercices)
             var text : EditText = view.findViewById(R.id.trainingTitle)
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
             val day = c.get(Calendar.DAY_OF_MONTH)
+
             var selectedDay : Int = day
             var selectedMonth : Int = month
             var selectedYear : Int = year
+
+            loadExercices()
             btn.setOnClickListener {
                 val dpd = DatePickerDialog(requireActivity(), DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     // Display Selected date in TextView
@@ -46,6 +53,9 @@ class DialogFragmentAddTraining() : DialogFragment(){
                     Log.d("TAG DATE SELECTIONEE", "$dayOfMonth / $month / $year")
                 }, year, month, day)
                 dpd.show()
+            }
+            btn2.setOnClickListener {
+                openDialog(requireActivity(),map)
             }
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
@@ -57,7 +67,7 @@ class DialogFragmentAddTraining() : DialogFragment(){
                         Log.d("Training name and date", "${text.text} on date : $selectedDay / $selectedMonth / $selectedYear")
                         var timestamp_date = Timestamp(selectedYear,selectedMonth,selectedDay,0,0,0,0)
 
-                        addTrainingToUser(text.text.toString(),timestamp_date)
+                        addTrainingToUser(text.text.toString(),timestamp_date,selectedItems)
                     })
                     /*
                 .setNegativeButton(R.string.cancel,
@@ -68,7 +78,7 @@ class DialogFragmentAddTraining() : DialogFragment(){
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
-    protected fun addTrainingToUser(text : String, date : Timestamp) {
+    protected fun addTrainingToUser(text: String, date: Timestamp, selectedItems: ArrayList<Int>) {
         val user = Firebase.auth.currentUser
         val uid = user?.uid
         val db = Firebase.firestore
@@ -76,11 +86,78 @@ class DialogFragmentAddTraining() : DialogFragment(){
             "title" to text,
             "date" to date
         )
-
+        val newExercises = ArrayList<Int>()
+        for (v in selectedItems){
+            Log.d("SELCTION",v.toString())
+        }
+        Log.d("JSUIS ICIIIIIIII",selectedItems.toString())
         db.collection("users").document("$uid").collection("trainings")
             .add(training)
-            .addOnSuccessListener { Log.d("TAG", "DocumentSnapshot successfully written!") }
+            .addOnSuccessListener { documentReference ->
+
+                for (i in selectedItems){
+                    Log.d("SELCTION",i.toString())
+                    val key_id = map.keys.toTypedArray()[i]
+                    val value_name = map.get(key_id)
+                    val exercice = hashMapOf(
+                        "name" to value_name
+                    )
+                    db.collection("users").document("$uid")
+                        .collection("trainings").document(documentReference.id)
+                        .collection("exercices").document("$key_id")
+                        .set(exercice)
+                }
+            }
             .addOnFailureListener { e -> Log.w("TAG", "Error writing document", e) }
 
+    }
+    private fun openDialog(fa: FragmentActivity, map: LinkedHashMap<String, String>) {
+        val array = mutableListOf<String>()
+        for (v in map){
+            array.add(v.value)
+        }
+        selectedItems.clear()
+        val alertDialogBuilder = AlertDialog.Builder(fa, R.style.MyDialogTheme)
+        alertDialogBuilder.setTitle(R.string.exercise_picker)
+            .setMultiChoiceItems(array.toTypedArray(), null,
+                DialogInterface.OnMultiChoiceClickListener { dialog, which, isChecked ->
+                    if (isChecked) {
+                        selectedItems.add(which)
+                    } else if (selectedItems.contains(which)) {
+                        selectedItems.remove(which)
+                    }
+                })
+            // Set the action buttons
+            .setPositiveButton(R.string.ok,
+                DialogInterface.OnClickListener { dialog, id ->
+                    Log.d("ICIIIII", selectedItems.toString())
+
+                })
+            .setNegativeButton(R.string.cancel,
+                DialogInterface.OnClickListener { dialog, id ->
+                })
+        val alert: AlertDialog = alertDialogBuilder.create()
+        alert.show()
+        //alert.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(R.color.colorPrimary)
+        //alert.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(R.color.colorPrimary)
+        //alert.getButton(DialogInterface.BUTTON_POSITIVE).setBackgroundColor(R.color.colorPrimary)
+        //alert.getButton(DialogInterface.BUTTON_NEGATIVE).setBackgroundColor(R.color.colorPrimary)
+        /*val newFragment = DialogExerciseSelector(array.toTypedArray(),map,fa)
+        newFragment.setTargetFragment(this, 0)
+        fragmentManager?.let { newFragment.show(it,"newfrag") }*/
+    }
+
+    fun loadExercices() {
+        val db = Firebase.firestore
+        val docRef = db.collection("exercises")
+        docRef.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    map[document.id] = document.getString("name").toString()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "get failed with ", exception)
+            }
     }
 }
