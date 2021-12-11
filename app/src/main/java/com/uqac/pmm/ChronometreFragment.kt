2347d24fabc02b6330f0de73a7d343e071547e0d
Sotial <tintos06358@gmail.com>
2021-12-11 14:34:11 +0100

    formatage du temps du chronomètre

--- app/src/main/java/com/uqac/pmm/ChronometreFragment.kt
+++ app/src/main/java/com/uqac/pmm/ChronometreFragment.kt
@@ -102,3 +102,42 @@ class StopwatchStateCalculator(
             is StopwatchState.Paused -> oldState
         }
 }
+
+class ElapsedTimeCalculator(
+    private val timestampProvider: TimestampProvider,
+) {
+
+    fun calculate(state: StopwatchState.Running): Long {
+        val currentTimestamp = timestampProvider.getMilliseconds()
+        val timePassedSinceStart = if (currentTimestamp > state.startTime) {
+            currentTimestamp - state.startTime
+        } else {
+            0
+        }
+        return timePassedSinceStart + state.elapsedTime
+    }
+}
+
+internal class TimestampMillisecondsFormatter() {
+
+    companion object {
+        const val DEFAULT_TIME = "00:00:000"
+    }
+
+    fun format(timestamp: Long): String {
+        val millisecondsFormatted = (timestamp % 1000).pad(3)
+        val seconds = timestamp / 1000
+        val secondsFormatted = (seconds % 60).pad(2)
+        val minutes = seconds / 60
+        val minutesFormatted = (minutes % 60).pad(2)
+        val hours = minutes / 60
+        return if (hours > 0) {
+            val hoursFormatted = (minutes / 60).pad(2)
+            "$hoursFormatted:$minutesFormatted:$secondsFormatted"
+        } else {
+            "$minutesFormatted:$secondsFormatted:$millisecondsFormatted"
+        }
+    }
+
+    private fun Long.pad(desiredLength: Int) = this.toString().padStart(desiredLength, '0')
+}
